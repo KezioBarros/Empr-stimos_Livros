2d40d71defd1c13e826d5c9ec99298287a9633ed
Kezio Pereira Barros <keziopb@gmail.com>
2024-01-22 15:57:09 -0300

    Ïniciando Site de Emprestimos de Livros

---
+++ EmprestimosLivro/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmprestimosLivro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimosLivro.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+             
+        }
+
+        public DbSet<EmprestimosModels> Emprestimos { get; set;}
+    }
+}
---
+++ EmprestimosLivro/Models/EmprestimosModels.cs
@@ -0,0 +1,13 @@
+﻿namespace EmprestimosLivro.Models
+{
+    public class EmprestimosModels
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataUtimaAtualizacao { get; set; } = DateTime.Now;
+
+
+    }
+}
